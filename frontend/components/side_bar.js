6a381e1b73c393b3dcec8ef2484f3411317e826e
Samuel Wonjae Lee <samuellee0220@gmail.com>
2024-09-28 12:09:02 -0400

    Fixed small error

--- frontend/components/side_bar.js
+++ frontend/components/side_bar.js
@@ -12,7 +12,7 @@ export default function Sidebar() {
         <div className="sidebar">
             <h2 className=" text-3xl font-extrabold p-4" style={{ color: '#4F46E5' }}>Textmaster</h2>
             <nav className="mt-8">
-                <ul class='purple-colored-text'>
+                <ul className='purple-colored-text'>
                      {['Home', 'Multiplayer', 'Singleplayer', 'Leaderboard', 'Shop'].map((item, index) => (
                         <li
                             key={index}
