8ddab566d8b339270be337e6f1b225a0713a459a
renzi1 <andy.ren@mail.utoronto.ca>
2024-09-28 13:26:31 -0400

    partly done messaging

---
+++ frontend/components/side_bar.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { useState } from "react";
+
+
+export default function Sidebar() {
+      const [activeIndex, setActiveIndex] = useState(null);
+
+    const handleClick = (index) => {
+        setActiveIndex(index);
+    };
+    return (
+        <div className="sidebar" style={{ borderRight: '2px solid #f3f3f3' }}>
+            <h2 className=" text-3xl font-extrabold p-4" style={{ color: '#4F46E5' }}>Textmaster</h2>
+            <nav className="mt-8">
+                <ul class='purple-colored-text'>
+                     {['Home', 'Multiplayer', 'Singleplayer', 'Leaderboard', 'Shop'].map((item, index) => (
+                        <li
+                            key={index}
+                            className={`p-4 font-extrabold`}
+                            style={{
+                                backgroundColor: activeIndex === index ? '#4F46E5' : 'transparent',
+                                color: activeIndex === index ? 'white' : '#4F46E5',
+                            }}
+                            onClick={() => handleClick(index)}
+                        >
+                            {item}
+                        </li>
+                    ))}
+                </ul>
+            </nav>
+        </div>
+    );
+}
