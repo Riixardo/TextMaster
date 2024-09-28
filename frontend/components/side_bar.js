54798e5d7b26f9b7bd3c49b6b1b93982ab606700
Samuel Wonjae Lee <samuellee0220@gmail.com>
2024-09-28 13:19:34 -0400

    Added user route for sidebar

--- frontend/components/side_bar.js
+++ frontend/components/side_bar.js
@@ -1,19 +1,29 @@
-import React from "react";
-import { useState } from "react";
+import React, { useState } from 'react';
+import { useRouter } from 'next/router';
 
+const Sidebar = () => {
+    const [activeIndex, setActiveIndex] = useState(null);
+    const router = useRouter();
 
-export default function Sidebar() {
-      const [activeIndex, setActiveIndex] = useState(null);
-
-    const handleClick = (index) => {
+    const handleClick = (index, path) => {
         setActiveIndex(index);
+        router.push(path);
     };
+
+    const menuItems = [
+        { name: 'Home', path: '/home' },
+        { name: 'Multiplayer', path: '/multiplayer/room' },
+        { name: 'Singleplayer', path: '/singleplayer' },
+        { name: 'Leaderboard', path: '/leaderboard' },
+        { name: 'Shop', path: '/shop' },
+    ];
+
     return (
         <div className="sidebar">
-            <h2 className=" text-3xl font-extrabold p-4" style={{ color: '#4F46E5' }}>Textmaster</h2>
+            <h2 className="text-3xl font-extrabold p-4" style={{ color: '#4F46E5' }}>Textmaster</h2>
             <nav className="mt-8">
                 <ul className='purple-colored-text'>
-                     {['Home', 'Multiplayer', 'Singleplayer', 'Leaderboard', 'Shop'].map((item, index) => (
+                    {menuItems.map((item, index) => (
                         <li
                             key={index}
                             className={`p-4 font-extrabold`}
@@ -21,13 +31,15 @@ export default function Sidebar() {
                                 backgroundColor: activeIndex === index ? '#4F46E5' : 'transparent',
                                 color: activeIndex === index ? 'white' : '#4F46E5',
                             }}
-                            onClick={() => handleClick(index)}
+                            onClick={() => handleClick(index, item.path)}
                         >
-                            {item}
+                            {item.name}
                         </li>
                     ))}
                 </ul>
             </nav>
         </div>
     );
-}
+};
+
+export default Sidebar;
