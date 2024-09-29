f72a76d7a5a62ce0de96b9f1bb7d3cb0d6c6ef80
Joanna Joy <mail.noosu@gmail.com>
2024-09-29 00:25:19 -0400

    added game over page for multiplayer, corrected buttons on join_or_create.js

--- frontend/pages/multiplayer/join_or_create.js
+++ frontend/pages/multiplayer/join_or_create.js
@@ -32,12 +32,12 @@ const JoinOrCreate = () => {
         <div className="min-h-screen w-screen" style={{ display: 'flex' }}>
             <Sidebar selectedButton={selectedButton} handleButtonClick={handleButtonClick} buttonWidth={buttonWidth} />
 
-            <div style={{ marginLeft: '20px', flex: 1, display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-                <h1 className="text-4xl mt-5">Join Or Create Lobby</h1>
+            <div style={{ backgroundColor: '#FFFFFF', marginLeft: '20px', flex: 1, display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+                <h1 className="text-4xl mt-5" style={{paddingTop:'20px'}}>Join Or Create Lobby</h1>
 
-                <div className="mt-5 flex space-x-4">
-                    <button className="px-16 py-2 #4F46E5 text-white text-lg rounded" onClick={handleJoinButton}>Join</button>
-                    <button className="px-16 py-4 #4F46E5 text-white text-lg rounded" onClick={handleCreateButton}>Create</button>
+                <div className="mt-5 flex space-x-4" style={{gap: '140px', paddingTop: '130px'}}>
+                    <button className="px-16 py-2 #4F46E5 text-white text-lg rounded" onClick={handleJoinButton} style={{padding: '40px', textAlign:"center"}}>Join</button>
+                    <button className="px-16 py-4 #4F46E5 text-white text-lg rounded" onClick={handleCreateButton} style={{padding: '40px', textAlign: "center"}}>Create</button>
                 </div>
             </div>
         </div>
