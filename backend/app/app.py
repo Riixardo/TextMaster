a4ae7867c4265c38097c9cac0c89f6b572381d65
renzi1 <andy.ren@mail.utoronto.ca>
2024-09-28 23:35:17 -0400

    testing messaging

--- backend/app/app.py
+++ backend/app/app.py
@@ -50,5 +50,57 @@ def handle_leave_room(data):
 def hello():
     return jsonify(message='Hello from Flask!')
 
+@app.route('/api/create_game', methods=['POST'])
+def create_game():
+    data = request.json
+    match_id = data['match_id']
+    player_list = data['player_list']
+    game_info = GameInfo()
+    game = game_info.create_game(match_id, player_list)
+    return jsonify({'message': 'Game created', 'game': game})
+
+@app.route('/api/update_score', methods=['POST'])
+def update_score():
+    data = request.json
+    match_id = data['match_id']
+    players = data['players']
+    new_scores = data['new_scores']
+    game_info = GameInfo()
+    game_info.update_score(match_id, players, new_scores)
+    return jsonify({'message': 'Score updated'})
+
+@app.route('/api/get_leaderboard', methods=['GET'])
+def get_leaderboard():
+    match_id = request.args.get('match_id')
+    if match_id in GameLeaderBoards:
+        return jsonify({'leaderboard': GameLeaderBoards[match_id]})
+    else:
+        return jsonify({'message': 'Match ID not found'}), 404
+
+#game_leaderboard = {"room111":{"frank": 10, "bob": 20}} 
+
+GameLeaderBoards = {}
+
+class GameInfo:
+    def create_game(self, match_id, player_list):
+        if match_id not in GameLeaderBoards:
+            GameLeaderBoards[match_id] = []
+            for player in player_list:
+                GameLeaderBoards[match_id][player] = 0
+            return GameLeaderBoards[match_id]
+        else:
+            return GameLeaderBoards[match_id]
+
+    # def find_game(self, match_id):
+    #     return GameLeaderBoards[match_id]
+        
+    def update_score(self,match_id, players, new_scores):
+        for scores in GameLeaderBoards[match_id]:
+            scores[1] = new_scores[players.index(scores[0])]
+
+    def end_game(self, match_id):
+        del GameLeaderBoards[match_id]
+
+
 if __name__ == '__main__':
     socketio.run(app, debug=True, port=5000)
